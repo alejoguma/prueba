03a725ef1994401fee2f852cc2c9b9992b77b93f
alejo <alejandroguma@gmail.com>
2019-04-05 15:19:40 -0500

    'v1'

---
+++ index.js
@@ -0,0 +1,25 @@
+//Dependencies
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+//Routes
+import AppRoutes from './routes'
+
+//import App from './components/App';
+
+//Assets
+import './index.css';
+
+
+
+render(
+	<Router>
+		<AppRoutes />	
+	</Router>,
+	document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
